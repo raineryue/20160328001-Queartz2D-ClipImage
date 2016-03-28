0fe5dc82c8f79f7caa43101b8f53647a344b72c7
raineryue <it_jinlei.yue@163.com>
2016-03-28 10:09:25 +0800

    Initial Commit

---
+++ 20160328001-Queartz2D-ClipImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160328001-Queartz2D-ClipImage
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160328001-Queartz2D-ClipImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160328001-Queartz2D-ClipImage
+//
+//  Created by Rainer on 16/3/28.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
